b5a8d0deed0ef4f22ca25db028c3548942b8e6ca
Thiago Pinto Braga <85633652+thiagopbraga@users.noreply.github.com>
2024-06-26 07:15:41 -0300

    Removing border bottom from tagline

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <header className=' p-4 text-white text-center flex flex-col items-center gap-4'>
       <Image loading='eager' src='/votegpt_logo.jpg' alt='logo' width={200} height={200} />
-      <span className='text-black border-b-2'>
+      <span className='text-black'>
         Know your candidates, lead the change
       </span>
     </header>
