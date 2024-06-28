004ec1d25787f2f7f5574284914466b0f047194b
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-06-27 21:38:51 -0300

    fix imports

--- src/app/Common/data/Countries.ts
+++ src/app/Common/data/Countries.ts
@@ -1,4 +1,4 @@
-import CountryType from '@/app/common/types/CountryType';
+import CountryType from '../types/CountryType';
 
 // From https://bitbucket.org/atlassian/atlaskit-mk-2/raw/4ad0e56649c3e6c973e226b7efaeb28cb240ccb0/packages/core/select/src/data/countries.js
 export const countries: readonly CountryType[] = [
