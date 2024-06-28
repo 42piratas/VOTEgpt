4fe9d819579a2bc73bfab82e6c3709507a83467f
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-06-27 22:37:59 -0300

    alter text color for @

--- src/app/Common/components/Footer.tsx
+++ src/app/Common/components/Footer.tsx
@@ -9,13 +9,13 @@ const Footer: React.FC = () => {
       <div className='flex flex-col gap-2 w-full items-center border-t p-4'>
         <p className='gap-5'>
           ❤️ Made possible by{' '}
-          <a href='https://www.akasha.org/' target='_blank' rel='noreferrer'>
+          <a href='https://www.akasha.org/' className='text-[#2592BF] underline hover:text-[#1a749b]' target='_blank' rel='noreferrer'>
             @AKASHA{' '}
           </a>
-          <a href='https://www.mozilla.org/' target='_blank' rel='noreferrer'>
+          <a href='https://www.mozilla.org/' className='text-[#2592BF] underline hover:text-[#1a749b]' target='_blank' rel='noreferrer'>
             @Mozilla{' '}
           </a>
-          <a href='https://ethereum.org/' target='_blank' rel='noreferrer'>
+          <a href='https://ethereum.org/' className='text-[#2592BF] underline hover:text-[#1a749b]' target='_blank' rel='noreferrer'>
             @Ethereum
           </a>
         </p>
