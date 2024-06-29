c9d979e94f16c618c66d08eb730aa36be4367952
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-06-29 00:16:28 -0300

    chore: Update candidate page header position to left

--- src/app/candidate/[id]/page.tsx
+++ src/app/candidate/[id]/page.tsx
@@ -8,7 +8,7 @@ export default function CandidatePage({ params }: { params: { id: string } }) {
   const realId = parseInt(params.id) - 1;
   return (
     <div className='flex flex-col min-h-screen justify-normal'>
-      <HeaderSecondary />
+      <HeaderSecondary position='left' />
       <div className='max-w-4xl mx-auto p-4 mb-28'>
         <div className='flex items-center space-x-4 mb-4'>
           <Image
