c6e84bc7c1834ad90f6c041f8f335cbc023c23a6
Ânderson Q <anquadros@gmail.com>
2024-08-01 05:53:58 -0300

    Reformats and fixes

--- gpt-prompts/prompts_candidate.py
+++ gpt-prompts/prompts_candidate.py
@@ -2,35 +2,35 @@ from prompts_common import *
 
 
 ####### SHORTNAME
-####### EX EXPECTED RETURN:
-####### 'Lula'
-####### - or -
-####### 'FALSE'
+####### E.G.
+# 'Lula'
+# 'Joe Biden'
+# FALSE
 
-candidate_shortname = (
+candidate_shortname: str = (
     "Please provide the commonly used short name or nickname for the political candidate named {candidate_name}. "
-    "Respond with only the short name or nickname. If there is no commonly used short name or nickname, respond with 'FALSE'."
+    "Respond with only the short name or nickname. If there is no commonly used short name or nickname, respond with FALSE."
 ).format(candidate_name=candidate_name)
 
 
 ####### EXPERIENCE
 
-candidates_experience = "Please, list {candidate_name}'s, the {candidate_nationality} candidate, previous political roles with dates, formatted as: '- Role (1999-2099)'"å
+candidates_experience: str = "Please, list {candidate_name}'s, the {candidate_nationality} candidate, previous political roles with dates, formatted as: '- Role (1999-2099)'"
 
 candidates_experience += " " + sources_reliable + " " + cite_sources
 
 ####### KEYWORDS
 
-candidate_keywords = "Please, return the 5 keywords that best describe {candidate_name}, the {candidate_nationality} candidate. Exclude any words related to professional roles, titles, or positions, such as 'politician', 'president', 'minister', etc."
+candidate_keywords: str = "Please, return the 5 keywords that best describe {candidate_name}, the {candidate_nationality} candidate. Exclude any words related to professional roles, titles, or positions, such as 'politician', 'president', 'minister', etc."
 
 ####### LGBTQ
 
-candidate_lgbtq = "In 140 characters-max, what's {candidate_name}'s, the {candidate_nationality} candidate, position on LGBTQ rights?"
+candidate_lgbtq: str = "In 140 characters-max, what's {candidate_name}'s, the {candidate_nationality} candidate, position on LGBTQ rights?"
 
 candidate_lgbtq += " " + sources_reliable + " " + clean_output + " " + cite_sources
 
 ####### LGBTQ SUMMARY
 
-candidate_lgbtq_sum = "Please, describe {candidate_name}'s, the {candidate_nationality} candidate, position on LGBTQ rights as either: 'SUPPORTIVE', 'OPPOSED', 'NEUTRAL', 'AMBIGUOUS', or 'UNKNOWN'."
+candidate_lgbtq_sum: str = "Please, describe {candidate_name}'s, the {candidate_nationality} candidate, position on LGBTQ rights as either: 'SUPPORTIVE', 'OPPOSED', 'NEUTRAL', 'AMBIGUOUS', or 'UNKNOWN'."
 
 candidate_lgbtq_sum += " " + sources_reliable + " " + clean_output + " " + cite_sources
--- gpt-prompts/prompts_election.py
+++ gpt-prompts/prompts_election.py
@@ -2,10 +2,10 @@ from prompts_common import *
 
 
 ####### CANDIDATES
-####### EXPECTED RETURN EX:
-####### [‘United States’, ‘Presidential’, ‘2024-11-05’, [‘Joe Biden’, ‘Donald Trump’, ‘Chase Oliver’, ‘Robert F. Kennedy Jr.’, ‘Nikki Haley’, ‘Ron DeSantis’, ‘Vivek Ramaswamy’, ‘Cornel West’, ‘Jill Stein’, ‘Larry Elder’], [‘Ballotpedia’, ‘Wikipedia’]]
+####### E.G.
+# [‘United States’, ‘Presidential’, ‘2024-11-05’, [‘Joe Biden’, ‘Donald Trump’, ‘Chase Oliver’, ‘Robert F. Kennedy Jr.’, ‘Ron DeSantis’,  ‘Cornel West’], [‘Ballotpedia’, ‘Wikipedia’]]
 
-election_candidates = (
+election_candidates: str = (
     "Please, provide a list of the full names of all candidates participating in the upcoming "
     "{election_type} elections in {country} in {election_date}. "
     "Format the response as follows: "
